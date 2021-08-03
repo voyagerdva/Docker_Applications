ee323a1522daf5aa2bc7609dae0bb6de609b69e8
Kirill Zhuravlev <48881889+voyagerdva@users.noreply.github.com>
2021-08-03 11:32:15 +0300

    Mongo project

---
+++ docker-mongo-example/app.py
@@ -0,0 +1,15 @@
+import time
+from uuid import uuid4
+from storage import MongodbService
+
+storage = MongodbService.get_instance()
+
+for _ in range(5):
+    dto = {
+        "_id": str(uuid4()),
+        "payload": str(uuid4())
+    }
+    storage.save_data(dto)
+
+for data in storage.get_data():
+    print(data)
